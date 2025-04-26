0e87ed02fb2aae07fd47dbc6be16400f71dc46ae
Dhruval Bhinsara <48351115+DhruvalBhinsara1@users.noreply.github.com>
2025-04-27 01:42:16 +0530

    added a video suggestion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -110,6 +110,7 @@ const Contact = () => {
                             >
                                 <option value="Race Predictions">Race Predictions</option>
                                 <option value="News Feedback">News Feedback</option>
+                                <option value="Make A Video">Make A Video</option>
                                 <option value="Roast Suggestions">Roast Suggestions</option>
                                 <option value="F1 Chat">F1 Chat</option>
                             </select>
